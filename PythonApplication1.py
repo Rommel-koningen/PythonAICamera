75ece00120aaf18b3a42172832acd1c854dcce2e
Ruby Vergouwen <rubenjvergouwen@gmail.com>
2025-06-23 13:17:45 +0200

    Verbinding met Azure API werkt

--- PythonApplication1.py
+++ PythonApplication1.py
@@ -1,4 +1,5 @@
 from asyncio.windows_events import NULL
+from datetime import datetime
 import cv2
 import ultralytics
 from ultralytics import YOLO
@@ -11,63 +12,11 @@ pipeline = dai.Pipeline()
 camRgb = pipeline.create(dai.node.ColorCamera)
 camRgb.setPreviewSize(640, 480)
 
-model_path = 'weights/best.pt'
+model_path = 'weights2/best.pt'
 model = YOLO(model_path)
 
 trashTypes = ['biodegradable', 'cardboard', 'glass', 'metal', 'paper', 'plastic']
-
-def detect_and_annotate(model, frame):
-    results = model.predict(frame, conf=0.6)
-    
-    for result in results:
-        for box in result.boxes:
-            x1, y1, x2, y2 = map(int, box.xyxy[0])
-            cls_id = int(box.cls[0])
-            label = trashTypes[cls_id]
-
-            # Draw box and label
-            cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
-            cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX,
-                        0.5, (255, 0, 0), 2)
-
-            # Create JSON data
-            detected_data = {
-                "label": label,
-                "coordinates": {
-                    "x1": x1,
-                    "y1": y1,
-                    "x2": x2,
-                    "y2": y2
-                }
-            }
-
-            data_to_send = {
-                "foto_Id": "",
-                "datum_En_Tijd": "2025-06-17T10:12:13.790Z",
-                "camera_Naam": "Cam1",
-                "longitude": 0,
-                "latitude": 0,
-                "postcode": "",
-                "windrichting": "",
-                "temperatuur": 0,
-                "weer_Omschrijving": "",
-                "afvalData": [
-                    {
-                        "afval_Id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-                        "afval_Type": "cardboard",
-                        "confidence": 0.70
-                    },
-                    {
-                        "afval_Id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-                        "afval_Type": "biodegradable",
-                        "confidence": 0.78
-                    }
-            ]}
-
-            # Send to API
-            send_to_api(detected_data)
-
-    return frame
+testingSending = True
 
 def detect_and_annotate2(model, frame):
     results = model.predict(frame, conf=0.6)
@@ -76,34 +25,35 @@ def detect_and_annotate2(model, frame):
 
         all_trash = []
 
-        all_trash.append({
-            "afval_Id": "",
-            "afval_Type": "Test",
-            "confidence": 0
-        })
+        if testingSending:
+            all_trash.append({
+                "afval_Id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+                "afval_Type": "TestBigMoment",
+                "confidence": 0
+            })
 
         for result in results:
             for box in result.boxes:
                 x1, y1, x2, y2 = map(int, box.xyxy[0])
                 cls_id = int(box.cls[0])
                 label = trashTypes[cls_id]
+                confi = float(box.conf)
 
                 cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
                 cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
                 
                 print("Categorie-ID: ", cls_id)
                 print("Categorie-naam: ", label)
-                # confi = model.confidence
 
                 all_trash.append({
-                    "afval_Id": "",
+                    "afval_Id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
                     "afval_Type": label,
-                    "confidence": 0
+                    "confidence": confi
                 })
 
         if all_trash != []:
             data_to_send = {
-                "foto_Id": "",
+                "foto_Id": "3fa85f64-5717-4562-b3fc-2c963f66afa6",
                 "datum_En_Tijd": "2025-06-17T10:12:13.790Z",
                 "camera_Naam": "Cam1",
                 "longitude": 0,
@@ -122,12 +72,12 @@ def detect_and_annotate2(model, frame):
     return frame
 
 def send_to_api(detected_object):
-    url = "http://localhost:5120/TrashdataSensoring"
-    headers = {"Content-Type": "application/json"}
+    url = "https://rommelkoningencontainer--rommelkoningencontainer.mangopebble-9c8f37e9.northeurope.azurecontainerapps.io/TrashdataSensoring"
+    headers = {"Api-Key-Name": "G3vVhpsno58iXRV5pmtZKwhQqd160jSietseiseenlandverraderPlKI0o3v07lAKyX9tp8BqtoxVgBEkUrGWxvrrtMdYRcbECin45227w7XU82PfyuFjkGW13MGxkvKJLZZ9YEvFzBGRtsGXpIkyvxzWSWPPZXUS7orI73OIHN8NszwuZqS25siCTYl6XVpgAKGnta4LgOTwC9jSQOHpy8PU9dNNQ", "Content-Type": "application/json"}
 
     try:
-        response = requests.post(url, headers=headers, data=json.dumps(detected_object))
-        if response.status_code == 200:
+        response = requests.post(url, headers=headers, json = detected_object)
+        if response.status_code == 200 or response.status_code == 204:
             print("Sent to API:", detected_object)
         else:
             print("Failed to send. Status:", response.status_code, response.text)
@@ -151,7 +101,7 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-    time.sleep(5)
+    time.sleep(10)
 
 cap.release()
 cv2.destroyAllWindows()
